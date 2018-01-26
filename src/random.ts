537bfee642c700c1d68ab984c27645dd217c01b8
Charlie Besalke <Charles.Besalke@vu.com>
2018-01-26 16:14:06 -0600

    Setting up gitignore and initial library. Will add tests

--- src/random.ts
+++ src/random.ts
@@ -1,3 +1,43 @@
 export namespace Random {
-    
+    function randomString():string {
+        var possibleChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+        var length = Math.floor(Math.random() * 50) + 1;
+
+        var result = "";
+        for(var i=0; i < length; i++) {
+            result += possibleChar.charAt(Math.floor((Math.random() * possibleChar.length)));
+        }
+
+        return result;
+    }
+
+    function randomNumber(): number {
+        return Math.floor(Math.random() * Number.MAX_VALUE);
+    }
+
+    function randomBoolean(): boolean {
+        return Math.floor(Math.random() * 2) === 0 ? true : false;
+    }
+
+    function randomObject<T>(typeData: new () => T): T {
+        var result = new typeData();
+        for(var i in Object.getOwnPropertyNames) {
+            var propertyType = typeof(result[i]);
+            switch (propertyType) {
+                case "number":
+                    result[i] = randomNumber();
+                    break;
+                case "boolean":
+                    result[i] = randomBoolean();
+                    break;
+                case "string":
+                    result[i] = randomString();
+                    break;
+                default:
+                    result[i] = {};
+                    break;
+            }
+
+        }
+    } 
 }
